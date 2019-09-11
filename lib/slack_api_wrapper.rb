9dac6fc38f3f318f4866b6bbef609813bf75111d
Samantha Coll <samcoll@Samanthas-MBP.localdomain>
2019-09-11 13:05:49 -0700

    created send_msg method, tests passing

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,10 +1,29 @@
 # slack_api_wrapper.rb
 require "httparty"
 
-module SlackApiWrapper
+class SlackApiError < StandardError; end
+
+module SlackApi
   BASE_URL = "https://slack.com/api/"
   API_KEY = ENV["SLACK_TOKEN"]
-
-  # Code goes here
-
-end
+  
+  def self.send_msg(message, channel)
+    
+    response = HTTParty.post(
+    "#{BASE_URL}/chat.postMessage",
+    
+    body:  {
+    token: API_KEY,
+    text: message,
+    channel: channel},
+    
+    headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+    
+    unless response.code == 200 && response.parsed_response["ok"]
+      raise SlackApiError, "Error when posting #{message} to #{channel}, error: #{response.parsed_response["error"]}"
+    end
+    
+    return true
+  end
+end 
